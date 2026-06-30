d51919b1c46ac90ac853b420d66b8d90a174ef28
maryearlvarat <maryearlmev@gmail.com>
2026-06-30 10:56:36 -0500

    this is the script to download data tables from google drive- should be done first evertyime

---
+++ scripts/first script run - download data from google drive.R
@@ -0,0 +1,32 @@
+#This is the start of my project code
+
+library(tidyverse)
+source("scripts/install_packages_function.R")
+
+lp("googledrive")
+lp("tidyverse")
+#BB1 Fall24----
+fid<-"https://drive.google.com/drive/folders/17jMX-KMDLhsykFaiaY5jqNiq9mAxknDI"
+folder_id = drive_get(as_id(fid))
+files =drive_ls(folder_id)
+
+dep<-"BB1_Fall2024"
+dir.create(paste0( "odata/",dep))
+for(i in 1:nrow(files)){
+  drive_download(file = files$id[i],
+               path = paste0("odata/",dep,"/",files$name[i]),
+               overwrite = TRUE) #drive download, file downloaded is whatever number of the loop we are on
+}
+#chnage FID and DEP to match what data set Im downloading
+#BB1 WInter 25----
+fid<-"https://drive.google.com/drive/folders/1ITxFkvxSvwwCID_oBsAuwf2no6EDJMdL"
+folder_id = drive_get(as_id(fid))
+files =drive_ls(folder_id)
+
+dep<-"BB1_Winter25"
+dir.create(paste0( "odata/",dep))
+for(i in 1:nrow(files)){
+  drive_download(file = files$id[i],
+                 path = paste0("odata/",dep,"/",files$name[i]),
+                 overwrite = TRUE) #drive download, file downloaded is whatever number of the loop we are on
+}
