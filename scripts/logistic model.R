cac1797e6e017d412268ea64b2e7344dbab8a48e
skraftarcher <sarcher@lumcon.edu>
2026-07-16 15:23:04 -0500

    logistic model comparing dolphin detections against environmental data

---
+++ scripts/logistic model.R
@@ -0,0 +1,55 @@
+# script to do logistic regression of detection vs environmental variables
+
+#load packages----
+source("scripts/install_packages_function.R")
+lp("tidyverse")
+lp("glmmTMB")
+lp("DHARMa")
+lp("ggeffects")
+
+
+# load data
+dts<-bind_rows(read.csv("wdata/combined_BB1fall24.csv"),
+               read.csv("wdata/combined_BB1winter25.csv"),
+               read.csv("wdata/combined_BB1spring25.csv"),
+               read.csv("wdata/combined_BB1summer25.csv"))
+
+#look to see if there is time structure
+ggplot(data=dts%>%mutate(dt=ymd_h(paste(yr,mnth,dy,hr))))+
+  geom_line(aes(x=dt,y=binary.dolphins))  
+
+ggplot(data=dts)+
+  geom_boxplot(aes(y=do.mg.l,fill=as.factor(binary.dolphins)))
+
+ggplot(data=dts)+
+  geom_point(aes(x=do.mg.l,y=binary.dolphins))
+
+ggplot(data=dts)+
+  geom_boxplot(aes(y=sal.ppt,fill=as.factor(binary.dolphins)))
+
+ggplot(data=dts)+
+  geom_point(aes(x=sal.ppt,y=binary.dolphins))
+
+ggplot(data=dts)+
+  geom_boxplot(aes(y=temp.c,fill=as.factor(binary.dolphins)))
+
+ggplot(data=dts)+
+  geom_point(aes(x=temp.c,y=binary.dolphins))
+
+pairs(dts[,c(7:9)])
+
+glmm.resids<-function(model){
+  t1 <- simulateResiduals(model)
+  print(testDispersion(t1))
+  plot(t1)
+}
+
+# binomial model
+log.mod<-glmmTMB(binary.dolphins~do.mg.l*sal.ppt+do.mg.l*temp.c+sal.ppt*temp.c,
+                 data=dts,
+                 family=binomial)
+glmm.resids(log.mod)
+
+summary(log.mod)
+
+plot(ggeffect(log.mod,terms=c("do.mg.l","temp.c")))
