1d6dddebd4b3d04cf7b120b74acd8a622cb2b47f
skraftarcher <sarcher@lumcon.edu>
2026-07-16 13:55:33 -0500

    turn environmental data pulling into a function

--- scripts/example_environmental_data_download.R
+++ scripts/example_environmental_data_download.R
@@ -15,34 +15,42 @@ bb2site<-"USGS-073802512"
 bb1site<-"USGS-07380249"
 tb1site<-"USGS-073813498"
 params=c("00480","00010","00300")# this is salinity (PPT), temperature (degrees C), and dissolved oxygen (mg/L)
-start.date<-as.POSIXct("2024-09-25 00:00:00",tz="America/Chicago")# change to match the start date/time of the deployment
-end.date<-as.POSIXct("2024-10-09 23:59:59",tz="America/Chicago")#change to match the end date/time of the deployment
 
 # download data
-env.data<-read_waterdata_continuous(monitoring_location_id = bb1site,#change to match site
-                          parameter_code = params,
-                          time=c(start.date,
-                                 end.date))
-# if you get a curl error -- update the curl and httr packages!
-# if you get a cli error -- update the cli package
-# can do updates when installing dataRetrieval package
+download.envdata<-function(start.date=NA,end.date=NA,site=NA){
+  env.data<-read_waterdata_continuous(monitoring_location_id = site,#change to match site
+                                      parameter_code = params,
+                                      time=c(start.date,
+                                             end.date))
+  # if you get a curl error -- update the curl and httr packages!
+  # if you get a cli error -- update the cli package
+  # can do updates when installing dataRetrieval package
+  
+  # organize data - note you should QAQC data before proceeding with this summarization step
+  env.data2<-env.data%>%
+    mutate(param=case_when(
+      parameter_code=="00480"~"sal.ppt",
+      parameter_code=="00010"~"temp.c",
+      parameter_code=="00300"~"do.mg.l"),#creates a variable that tells you what parameter is recorded
+      yr=year(time),
+      mnth=month(time),
+      dy=day(time),
+      hr=hour(time))%>%
+    group_by(yr,mnth,dy,hr,param)%>%
+    summarize(value=median(value,na.rm=T))%>% # taking the median measurement over each hour
+    pivot_wider(names_from = param,values_from = value,values_fill = NA)
+  
+  
+  return(env.data2)
+}
 
-# organize data - note you should QAQC data before proceeding with this summarization step
-env.data2<-env.data%>%
-  mutate(param=case_when(
-    parameter_code=="00480"~"sal.ppt",
-    parameter_code=="00010"~"temp.c",
-    parameter_code=="00300"~"do.mg.l"),#creates a variable that tells you what parameter is recorded
-  yr=year(time),
-  mnth=month(time),
-  dy=day(time),
-  hr=hour(time))%>%
-  group_by(yr,mnth,dy,hr,param)%>%
-  summarize(value=median(value,na.rm=T))%>% # taking the median measurement over each hour
-  pivot_wider(names_from = param,values_from = value,values_fill = NA)
 
 
+# trying function with bb1 and fall 24 dates
+fall24.env<-download.envdata(start.date=as.POSIXct("2024-09-25 00:00:00",tz="America/Chicago"),# change to match the start date/time of the deployment
+                             end.date=as.POSIXct("2024-10-09 23:59:59",tz="America/Chicago"),#change to match the end date/time of the deployment
+                             site=bb1site)
 
 # save the dataset
-write.csv(env.data2,"wdata/PICKADESCRIPTIVEFILENAME.csv",row.names=F)
+write.csv(fall24.env,"wdata/fall24_env.csv",row.names=F)
 
