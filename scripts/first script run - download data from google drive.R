1583beb707e29f732033b7df538c2030279da71e
maryearlvarat <maryearlmev@gmail.com>
2026-07-16 14:25:22 -0500

    all BB1 and BB2 sel tables downloaded

--- scripts/first script run - download data from google drive.R
+++ scripts/first script run - download data from google drive.R
@@ -3,6 +3,7 @@
 library(tidyverse)
 source("scripts/install_packages_function.R")
 
+
 lp("googledrive")
 lp("tidyverse")
 #BB1 Fall24----
@@ -42,3 +43,64 @@ for(i in 1:nrow(files)){
                  path = paste0("odata/",dep,"/",files$name[i]),
                  overwrite = TRUE) #drive download, file downloaded is whatever number of the loop we are on
 }
+#BB1 Spring 25
+fid<-"https://drive.google.com/drive/folders/1oNIcQi-wXeCWeKiJhfNlcalXD5unJaJJ"
+folder_id = drive_get(as_id(fid))
+files =drive_ls(folder_id)
+
+dep<-"BB1_Spring25"
+dir.create(paste0( "odata/",dep))
+for(i in 1:nrow(files)){
+  drive_download(file = files$id[i],
+                 path = paste0("odata/",dep,"/",files$name[i]),
+                 overwrite = TRUE) #drive download, file downloaded is whatever number of the loop we are on
+}
+#BB2 Fall 24
+  fid<-"https://drive.google.com/drive/folders/1NKVinEbOo6T5Absrd6tnTWNv3KyZfbKL"
+  folder_id = drive_get(as_id(fid))
+  files =drive_ls(folder_id)
+  
+  dep<-"BB2_Fall24"
+  dir.create(paste0( "odata/",dep))
+  for(i in 1:nrow(files)){
+    drive_download(file = files$id[i],
+                   path = paste0("odata/",dep,"/",files$name[i]),
+                   overwrite = TRUE) #drive download, file downloaded is whatever number of the loop we are on 
+}
+#BB2 Winter 25
+  fid<-"https://drive.google.com/drive/folders/1-JpPi8v4F4R8kL6Of8LvpoVqaVplEf82"
+  folder_id = drive_get(as_id(fid))
+  files =drive_ls(folder_id)
+  
+  dep<-"BB2_Winter25"
+  dir.create(paste0( "odata/",dep))
+  for(i in 1:nrow(files)){
+    drive_download(file = files$id[i],
+                   path = paste0("odata/",dep,"/",files$name[i]),
+                   overwrite = TRUE) #drive download, file downloaded is whatever number of the loop we are on 
+  } 
+#BB2 Spring 25
+  fid<-"https://drive.google.com/drive/folders/1JoyAu7c9sID2ORYUAmdYOlta33y7Ev7J"
+  folder_id = drive_get(as_id(fid))
+  files =drive_ls(folder_id)
+  
+  dep<-"BB2_Spring25"
+  dir.create(paste0( "odata/",dep))
+  for(i in 1:nrow(files)){
+    drive_download(file = files$id[i],
+                   path = paste0("odata/",dep,"/",files$name[i]),
+                   overwrite = TRUE) #drive download, file downloaded is whatever number of the loop we are on 
+  }   
+#BB2 Summer25
+  fid<-"https://drive.google.com/drive/folders/1oM4yf2khWQ3_y_RhAvLK0eE5hpXVdNXu"
+  folder_id = drive_get(as_id(fid))
+  files =drive_ls(folder_id)
+  
+  dep<-"BB2_Summer25"
+  dir.create(paste0( "odata/",dep))
+  for(i in 1:nrow(files)){
+    drive_download(file = files$id[i],
+                   path = paste0("odata/",dep,"/",files$name[i]),
+                   overwrite = TRUE) #drive download, file downloaded is whatever number of the loop we are on 
+  }    
+  
