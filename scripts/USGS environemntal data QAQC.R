d549a4b1ab826a4013822dd53cb63d167e3c2df4
maryearlvarat <maryearlmev@gmail.com>
2026-07-22 11:06:39 -0500

    usgs envi data qaqc

---
+++ scripts/USGS environemntal data QAQC.R
@@ -0,0 +1,15 @@
+# USGS Environemntal Data QAQC
+#summary
+summary(bb1fall24_env)
+summary(bb1winter25_env)
+summary(bb1spring25_env)
+summary(bb1summer25_env)
+summary(bb2fall24_env)
+summary(bb2winter25_env)
+summary(bb2spring25_env)
+summary(bb2summer25_env)
+summary(tb1fall24_env)
+summary(tb1winter25_env)
+summary(tb1spring25_env)
+summary(tb1summer25_env)
+
