22fb00d3c5c48af91ea1b494a06319f592928698
maryearlvarat <maryearlmev@gmail.com>
2026-07-22 10:42:18 -0500

    read all env data script

---
+++ scripts/read in all env data script.R
@@ -0,0 +1,31 @@
+# Read in all data Script
+
+
+library(readxl)
+#BB1 
+bb1fall24<-read.csv(file = "wdata/bb1fall24_env.csv")
+
+bb1winter25<-read.csv(file = "wdata/bb1winter25_env.csv")
+
+bb1spring25<-read.csv(file = "wdata/spring25_env.csv")
+
+bb1summer25<-read.csv(file = "wdata/bb1summer25_env.csv")
+
+#BB2
+bb2fall24<-read.csv(file = "wdata/bb2fall24_env.csv")
+
+bb2winter25<-read.csv(file = "wdata/bb2winter25_env.csv")
+
+bb2spring25<-read.csv(file = "wdata/bb2spring25_env.csv")
+
+bb2summer25<-read.csv(file = "wdata/bb2summer25_env.csv")
+
+#TB1
+tb1fall24<-read.csv(file = "wdata/tb1fall24_env.csv")
+
+tb1winter25<-read.csv(file = "wdata/tb1winter25_env.csv")
+
+tb1spring25<-read.csv(file = "wdata/tb1spring25_env.csv")
+
+tb1summer25<-read.csv(file = "wdata/tb1summer25_env.csv")
+
