6131b8c35417a9e97f6fe521914a827b027076e8
skraftarcher <sarcher@lumcon.edu>
2026-07-29 13:43:37 -0500

    fixed file names and column reordering

--- scripts/04_join_envi_and_dolphin_script.R
+++ scripts/04_join_envi_and_dolphin_script.R
@@ -124,7 +124,7 @@ LUMO6summer25<-left_join(LUMO6summer25_dolphins,LUMO6_env)%>%
   select(-deploy)
 summary(colnames(LUMO6summer25)%in% cn)
 
-LUMO6summer25_combined<-bind_rows(LUMO6summer25[0,],LUMO6summer25_combined)
+LUMO6summer25_combined<-bind_rows(BB2summer25[0,],LUMO6summer25)# lines like this are only to put the columns in the same order
 write.csv(LUMO6summer25_combined,"wdata/LUMO6summer25_combined.csv",row.names = F)
 
 #LUMO WINTER
@@ -135,7 +135,7 @@ LUMO6winter25<-left_join(LUMO6winter25_dolphins,LUMO6_env)%>%
          deployment="Winter")%>%
   select(-deploy)
 summary(colnames(LUMO6winter25)%in% cn)
-LUMO6winter25_combined<-bind_rows(LUMO6winter25[0,],LUMO6summer25)
+LUMO6winter25_combined<-bind_rows(LUMO6summer25[0,],LUMO6winter25)
 write.csv(LUMO6winter25_combined,"wdata/LUMO6winter25_combined.csv",row.names = F)
 
 #LUMOFALL
@@ -143,41 +143,44 @@ LUMO6_env<-read.csv("wdata/LUMO6_env.csv")
 LUMO6fall24_dolphins<-read.csv("wdata/LUMO6fall24_dolphins.csv")
 LUMO6fall24<-left_join(LUMO6fall24_dolphins,LUMO6_env)%>%
   mutate(site="LUMO6",
-         deployment="fall")%>%
+         deployment="Fall")%>%
   select(-deploy)
 summary(colnames(LUMO6fall24)%in% cn)
-LUMO6fall24_combined<-bind_rows(LUMO6fall24[0,],LUMO6winter25)
+LUMO6fall24_combined<-bind_rows(LUMO6winter25[0,],LUMO6fall24)
 write.csv(LUMO6fall24_combined,"wdata/LUMO6fall24_combined.csv",row.names = F)
+
 #LUMOSpring
 LUMO6_env<-read.csv("wdata/LUMO6_env.csv")
 LUMO6spring25_dolphins<-read.csv("wdata/LUMO6spring25_dolphins.csv")
 LUMO6spring25<-left_join(LUMO6spring25_dolphins,LUMO6_env)%>%
   mutate(site="LUMO6",
-         deployment="spring")%>%
+         deployment="Spring")%>%
   select(-deploy)
 summary(colnames(LUMO6spring25)%in% cn)
-LUMO6spring25_combined<-bind_rows(LUMO6spring25[0,],LUMO6winter25)
+LUMO6spring25_combined<-bind_rows(LUMO6winter25[0,],LUMO6spring25)
 write.csv(LUMO6spring25_combined,"wdata/LUMO6spring25_combined.csv",row.names = F)
 
 #TB1
 #TB1 Winter
-TB1_env<-read.csv("wdata/TB1_env.csv")
+
+## NOT WORKING START HERE
+TB1winter_env<-read.csv("wdata/TB1winter25_env.csv")
 TB1winter25_dolphins<-read.csv("wdata/TB1winter25_dolphins.csv")
-TB1winter25_combined<-left_join(TB1winter25_dolphins,TB1_env)%>%
+TB1winter25_combined<-left_join(TB1winter25_dolphins,TB1winter_env)%>%
   mutate(site="TB1",
-         deployment="fall")%>%
-  select(-deploy)
+         deployment="Winter",
+         do.mg.l=NA)
 summary(colnames(TB1winter25_combined)%in% cn)
-TB1winter25_combined<-bind_rows(summer24[0,],TB1winter25_combined)
+TB1winter25_combined<-bind_rows(BB1spring25[0,],TB1winter25_combined)
 write.csv(TB1winter25_combined,"wdata/TB1winter25_combined.csv",row.names = F)
 
 #TB1 Summer
-TB1_env<-read.csv("wdata/TB1_env.csv")
+TB1summer_env<-read.csv("wdata/TB1summer25_env.csv")
 TB1summer25_dolphins<-read.csv("wdata/TB1summer25_dolphins.csv")
-TB1summer25_combined<-left_join(TB1summer25_dolphins,TB1_env)%>%
+TB1summer25_combined<-left_join(TB1summer25_dolphins,TB1summer_env)%>%
   mutate(site="TB1",
-         deployment="fall")%>%
-  select(-deploy)
+         deployment="Summer",
+         do.mg.l=NA)
 summary(colnames(TB1summer25_combined)%in% cn)
-TB1summer25_combined<-bind_rows(summer24[0,],TB1summer25_combined)
+TB1summer25_combined<-bind_rows(BB1spring25[0,],TB1summer25_combined)
 write.csv(TB1summer25_combined,"wdata/TB1summer25_combined.csv",row.names = F)
